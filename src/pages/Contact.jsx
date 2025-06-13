c3de6fed8c7f57213a62443b59a4cd8e10d155ef
joshuakitong <joshuakitong@gmail.com>
2025-06-13 08:35:45 +0800

    feat: add bg texture and minor ui changes

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,9 +1,14 @@
 import { motion } from 'framer-motion';
+import { useEffect } from 'react';
 
 export default function ContactPage() {
+  useEffect(() => {
+      window.scrollTo(0, 0);
+    }, []);
+
   return (
-    <motion.div initial={{ opacity: 0, y: 30 }} animate={{ opacity: 1, y: 0 }} exit={{ opacity: 0, y: -30 }} transition={{ duration: 0.5 }} className="p-6">
-      <h1 className="text-3xl font-bold">Contact</h1>
+    <motion.div initial={{ opacity: 0, y: -30 }} animate={{ opacity: 1, y: 0 }} exit={{ opacity: 0, y: -30 }} transition={{ duration: 0.5 }} className="bg-texture h-screen">
+      <h1 className="text-3xl font-bold p-6">Contact</h1>
     </motion.div>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,14 @@
 import { motion } from 'framer-motion';
+import { useEffect } from 'react';
 
 export default function HomePage() {
+  useEffect(() => {
+      window.scrollTo(0, 0);
+    }, []);
+
   return (
-    <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }} className="p-6">
-      <h1 className="text-3xl font-bold">Home</h1>
+    <motion.div initial={{ opacity: 0, y: -30 }} animate={{ opacity: 1, y: 0 }} exit={{ opacity: 0, y: -30 }} transition={{ duration: 0.5 }} className="bg-home h-screen">
+      <h1 className="text-3xl font-bold p-6">Home</h1>
     </motion.div>
   );
 }
