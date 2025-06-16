8d7faf2bf7d90dc2879c9e784d34e67a982ba1d1
joshuakitong <joshuakitong@gmail.com>
2025-06-17 05:42:21 +0800

    fix: remove rounded border on mobile nav bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -42,7 +42,7 @@ export default function Navbar() {
       <div
         className={`md:flex md:items-center md:space-x-4 ${
           menuOpen ? 'block' : 'hidden'
-        } md:block absolute md:static top-12 left-0 w-full md:w-auto bg-dark md:bg-transparent p-4 md:p-0 border-b border-white md:border-0 rounded-md z-40`}
+        } md:block absolute md:static top-12 left-0 w-full md:w-auto bg-dark md:bg-transparent p-4 md:p-0 border-b border-white md:border-0 z-40`}
       >
         {navLinks.map((link) => (
           <Link
