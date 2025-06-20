65cc3ae1c3381a7ac6f937d2a769056e44823dd4
joshuakitong <joshuakitong@gmail.com>
2025-06-20 22:02:27 +0800

    refactor: make code content more readable

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -12,8 +12,13 @@ const skills = [
     summary: (
     <>
       I graduated with a Bachelor of Science in <strong>Information Technology</strong>, majoring in <strong>Software Development</strong>, in 2017.<br /><br />
-      In 2018, I worked as a <strong>Front-End Web Developer</strong> at Qualis Consulting Inc., developing web-based systems for U.S. utility cooperatives using <strong>ASP.NET (C#)</strong>, <strong>AngularJS</strong>, <strong>HTML</strong>, <strong>CSS</strong>, and <strong>Kendo UI</strong>. I also trained in <strong>Angular</strong> and gained some back-end experience with <strong>PL/SQL</strong>.<br /><br />
-      In 2021, I shifted to pursuing creative projects full-time and paused my developer career. In mid-2025, I decided to return to development and have been catching up on newer technologies — mainly <strong>React</strong>, <strong>Angular</strong>, and <strong>Python</strong> — through hands-on projects.
+
+      In 2018, I worked as a <strong>Front-End Web Developer</strong> at Qualis Consulting Inc., developing web-based systems for U.S. utility cooperatives 
+       using <strong>ASP.NET (C#)</strong>, <strong>AngularJS</strong>, <strong>HTML</strong>, <strong>CSS</strong>, and <strong>Kendo UI</strong>. I also trained 
+       in <strong>Angular</strong> and gained some back-end experience with <strong>PL/SQL</strong>.<br /><br />
+
+      In 2021, I shifted to pursuing creative projects full-time and paused my developer career. In mid-2025, I decided to return to development and have been catching up 
+       on newer technologies — mainly <strong>React</strong>, <strong>Angular</strong>, and <strong>Python</strong> — through hands-on projects.
     </>
     ),
     link: "/webdev",
@@ -24,8 +29,13 @@ const skills = [
     icon: <AudioLines size={48} />,
     summary: (
     <>
-      My creative journey began during the early stages of the pandemic in 2020, when I rediscovered my passion for music. Around that time, my brother and his friends launched a record label and collective called BOX84, and I’ve been involved in the full production process ever since.<br /><br />
-      I’ve learned to <strong>make beats</strong>, <strong>write and arrange songs</strong>, <strong>mix and master tracks</strong>, <strong>edit videos and graphics</strong>, <strong>release music across various platforms</strong>, and <strong>manage social media pages</strong>. I primarily use <strong>FL Studio</strong>, <strong>Adobe Premiere Pro</strong>, and <strong>Photoshop</strong>.<br /><br />
+      My creative journey began during the early stages of the pandemic in 2020, when I rediscovered my passion for music. Around that time, my brother and his friends 
+       launched a record label and collective called BOX84, and I’ve been involved in the full production process ever since.<br /><br />
+
+      I’ve learned to <strong>make beats</strong>, <strong>write and arrange songs</strong>, <strong>mix and master tracks</strong>, <strong>edit videos and graphics</strong>
+       , <strong>release music across various platforms</strong>, and <strong>manage social media pages</strong>. I primarily use <strong>FL Studio</strong>
+       , <strong>Adobe Premiere Pro</strong>, and <strong>Photoshop</strong>.<br /><br />
+
       Since then, I’ve produced over a hundred tracks, generating hundreds of thousands of streams across platforms.
     </>
     ),
@@ -37,9 +47,14 @@ const skills = [
     icon: <Gamepad2 size={48} />,
     summary: (
     <>
-      I’ve been a gamer for as long as I can remember — it’s something I’ve always loved and dreamt of turning into a career. It’s also where I first began developing my <strong>problem-solving</strong> and <strong>critical thinking skills</strong>.<br /><br />
-      In 2021, when NFT gaming surged during the altcoin season, I didn’t hesitate. I pursued my creative career full-time, became a <strong>competitive Axie Infinity player</strong>, and a <strong>coach</strong> and <strong>manager</strong> to a few scholars.<br /><br />
-      A year later, as the NFT hype faded, I transitioned into being a <strong>Twitch streamer</strong> and <strong>content creator</strong> as Kits2g. I built a following of thousands, streamed to hundreds per session, and regularly hosted community events, giveaways, and marathon streams.
+      I’ve been a gamer for as long as I can remember — it’s something I’ve always loved and dreamt of turning into a career. It’s also where I first began developing 
+       my <strong>problem-solving</strong> and <strong>critical thinking skills</strong>.<br /><br />
+
+      In 2021, when NFT gaming surged during the altcoin season, I didn’t hesitate. I pursued my creative career full-time, became 
+       a <strong>competitive Axie Infinity player</strong>, and a <strong>coach</strong> and <strong>manager</strong> to a few scholars.<br /><br />
+
+      A year later, as the NFT hype faded, I transitioned into being a <strong>Twitch streamer</strong> and <strong>content creator</strong> as Kits2g. I built a following 
+       of thousands, streamed to hundreds per session, and regularly hosted community events, giveaways, and marathon streams.
     </>
     ),
     link: "/gaming",
@@ -51,7 +66,9 @@ const skills = [
     summary: (
     <>
       I’ve always been fascinated by coloring, painting, and mixing colors, though I focused more on developing other talents growing up.<br /><br />
-      When our family business, Crocheted by Carisse, began to boom, I knew my eye for combining colors would come in handy. Since 2022, I’ve been actively <strong>creating new color combinations</strong> and <strong>producing fine cotton yarns</strong> for our hundreds of beloved customers worldwide.
+
+      When our family business, Crocheted by Carisse, began to boom, I knew my eye for combining colors would come in handy. Since 2022, I’ve been actively <strong>creating 
+        new color combinations</strong> and <strong>producing fine cotton yarns</strong> for our hundreds of beloved customers worldwide.
     </>
     ),
     link: "/yarn",
@@ -62,7 +79,10 @@ const skills = [
     icon: <Sparkles size={48} />,
     summary: (
     <>
-      Other roles I’ve taken on include <strong>sound technician</strong> for our family’s sound system business, <strong>graphic designer</strong> for small organizations like GDG Baguio, <strong>transcriptionist</strong> for Insight Asia, <strong>teacher</strong> and <strong>coach</strong> in both gaming and music, and <strong>cryptocurrency trader</strong>.<br /><br />
+      Other roles I’ve taken on include <strong>sound technician</strong> for our family’s sound system business, <strong>graphic designer</strong> for small organizations 
+       like GDG Baguio, <strong>transcriptionist</strong> for Insight Asia, <strong>teacher</strong> and <strong>coach</strong> in both gaming and music, 
+       and <strong>cryptocurrency trader</strong>.<br /><br />
+
       You can also learn more about my various interests and hobbies — books I read, shows I watch, and games I play — in this section.
     </>
     ),
