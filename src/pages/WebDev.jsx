313cefb413c68f20c9d1dc82cc4d53ab8b16e6b7
joshuakitong <joshuakitong@gmail.com>
2025-06-25 04:02:12 +0800

    docs: webdev update content

--- src/pages/WebDev.jsx
+++ src/pages/WebDev.jsx
@@ -38,9 +38,9 @@ const sections = [
           In June 2025, I decided to shift my focus back to development. I’ve been rebuilding my coding foundation and catching up on modern technologies — 
            mainly <strong>React</strong>, <strong>Angular</strong>, and <strong>Python</strong> — through interactive platforms like freeCodeCamp, solving challenges on 
            LeetCode, leveraging AI tools, and building hands-on projects. This portfolio is one of those projects, built with <strong>React</strong>
-           , <strong>Tailwind CSS</strong>, and <strong>Framer Motion</strong>. Visit the Projects section below to learn more.
+           , <strong>Tailwind CSS</strong>, and <strong>Framer Motion</strong>. Scroll down to the Projects section below to learn more.
         </p>
-        <h2 className="text-xl font-bold mb-2 tracking-wide text-gray-200">Web Dev – Tech & Tools</h2>
+        <h2 className="text-xl font-bold mb-2 tracking-wide text-gray-200">Tech & Tools</h2>
         <p>
           These are the technologies and tools I have experience with as a web developer:<br/>
         </p>
@@ -71,19 +71,20 @@ const sections = [
         </p>
         <div className="grid grid-cols-1 md:grid-cols-2 mt-2">
           <div>
-            <a href="https://joshuakitong.github.io/angular-to-do-app" target="_new" className="text-blue-400 hover:underline visited:text-purple-400">
+            <a href="https://joshuakitong.github.io/angular-to-do-app" target="_blank" className="text-blue-400 hover:underline visited:text-purple-400">
               <strong>To-do App</strong>
             </a><br/>
             <p className="ml-4 mr-4">
-              A simple to-do app built with <strong>Angular</strong> and <strong>Material Design</strong>, showcasing CRUD functionalities.
+              A to-do app built with <strong>Angular</strong> and <strong>Material Design</strong>, featuring CRUD operations, drag-and-drop functionality, 
+               confirmation modals, and toast notifications.
             </p>
-            <a href="https://joshuakitong.github.io/piano-app" target="_new" className="text-blue-400 hover:underline visited:text-purple-400">
+            <a href="https://joshuakitong.github.io/piano-app" target="_blank" className="text-blue-400 hover:underline visited:text-purple-400">
               <strong>Piano App</strong>
             </a><br/>
             <p className="ml-4 mr-4">
-              A simple piano app built with <strong>React</strong> and <strong>Vite</strong>, featuring a virtual keyboard with a simple control panel.
+              A piano app built with <strong>React</strong> and <strong>Vite</strong>, featuring a virtual keyboard with computer keyboard input and a simple control panel.
             </p>
-            <a href="https://joshuakitong.github.io/portfolio" target="_new" className="text-blue-400 hover:underline visited:text-purple-400">
+            <a href="https://joshuakitong.github.io/portfolio" target="_blank" className="text-blue-400 hover:underline visited:text-purple-400">
               <strong>My Portfolio</strong>
             </a><br/>
             <p className="ml-4 mr-4">
@@ -92,31 +93,31 @@ const sections = [
             </p>
           </div>
           <div>
-            <a href="https://flask-weather-app-k7b3.onrender.com" target="_new" className="text-blue-400 hover:underline visited:text-purple-400">
+            <a href="https://flask-weather-app-k7b3.onrender.com" target="_blank" className="text-blue-400 hover:underline visited:text-purple-400">
               <strong>Weather App</strong>
             </a><br/>
             <p className="ml-4 mr-4">
-              A simple weather app built with <strong>Python</strong> and <strong>Flask</strong>, where users are able to search for the weather by city.<br/>
+              A weather app built with <strong>Python</strong> and <strong>Flask</strong>, where users are able to search for the weather by city.<br/>
               <i className="text-xs text-gray-400">Note: The app may take a few seconds to load as it’s hosted on Render.com’s free tier.</i>
             </p>
-            <a href="https://joshuakitong.github.io/react-tic-tac-toe" target="_new" className="text-blue-400 hover:underline visited:text-purple-400">
+            <a href="https://joshuakitong.github.io/react-tic-tac-toe" target="_blank" className="text-blue-400 hover:underline visited:text-purple-400">
               <strong>Tic-Tac-Toe</strong>
             </a><br/>
             <p className="ml-4 mr-4">
               My first <strong>React</strong> project which is based on the 
-               official <a href="https://react.dev/learn/tutorial-tic-tac-toe" target="_new" className="text-blue-400 hover:underline visited:text-purple-400">React 
+               official <a href="https://react.dev/learn/tutorial-tic-tac-toe" target="_blank" className="text-blue-400 hover:underline visited:text-purple-400">React 
                Tic-Tac-Toe tutorial</a> with additional features and styling.
             </p>
-            <a href="https://joshuakitong.github.io/crocheted-by-carisse" target="_new" className="text-blue-400 hover:underline visited:text-purple-400">
+            <a href="https://joshuakitong.github.io/crocheted-by-carisse" target="_blank" className="text-blue-400 hover:underline visited:text-purple-400">
               <strong>Croched by Carisse</strong>
             </a><br/>
             <p className="ml-4 mr-4">
-              My first website since returning to development: A responsive, static product landing page of Crocheted by Carisse.
+              My first website since returning to development: A responsive, static product landing page for Crocheted by Carisse, built with HTML, CSS, and minimal JavaScript.
             </p>
           </div>
         </div>
         <p className="mt-2">
-          You can also visit my <a href="https://github.com/joshuakitong" target="_new" className="text-blue-400 hover:underline visited:text-purple-400">GitHub</a> to 
+          You can also visit my <a href="https://github.com/joshuakitong" target="_blank" className="text-blue-400 hover:underline visited:text-purple-400">GitHub</a> to 
           view source code and stay updated on future projects.
         </p>
       </>
