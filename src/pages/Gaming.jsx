017109521d8ec3eacfab57873c8f992545ada73b
joshuakitong <joshuakitong@gmail.com>
2025-06-25 23:10:26 +0800

    docs: gaming text content

--- src/pages/Gaming.jsx
+++ src/pages/Gaming.jsx
@@ -9,15 +9,41 @@ const sections = [
       <>
         <h2 className="text-xl font-bold mb-2 tracking-wide text-gray-200">Gaming Background</h2>
         <p className="mb-6">
-          (*work in progress: gaming background goes here)
+          I’ve always loved playing games growing up — from board and card games to physical sports. But it’s in video games and esports where I’ve found the most excitement and 
+           developed the strongest skill. It’s also where my <strong>problem-solving</strong> and <strong>critical thinking</strong> were honed the most.<br/><br/>
+
+          As a kid, I played a variety of RPGs, simulation, and puzzle games. During my elementary years, I started getting into PvP games, though back then it was 
+           all just for fun. It wasn’t until early college that I realized how good I actually was — local indie esports teams started reaching out, and I began competing in 
+           small tournaments for various MOBA and FPS/shooter games. Still, I didn’t take it too seriously at the time and mostly played for enjoyment.<br/><br/>
+
+          That changed during the 2021 altcoin season, when NFT games were booming — I decided to resign from my developer job to fully pursue my creative careers and 
+           become a <strong>professional Axie Infinity player</strong>, competing in high-ranking ladders and various tournaments.
+
         </p>
-        <h2 className="text-xl font-bold mb-2 tracking-wide text-gray-200">Competitive Gaming</h2>
+        <h2 className="text-xl font-bold mb-2 tracking-wide text-gray-200">Competitive Experience</h2>
         <p className="mb-6">
-          (*work in progress: competitive gaming goes here)
+          I’ve always had a competitive drive when it comes to gaming. Even though I wasn’t technically a pro in most of the games I played, I consistently ranked 
+           high — often reaching leaderboards on most of the games I played.<br/><br/>
+          
+          I began seriously considering a professional gaming career during my first month of playing Axie Infinity. Even with just an average team composition, I managed 
+           to break into the Top 5000, which motivated me to invest in stronger assets and fully commit to the game. From that point on, I consistently placed within 
+           the <strong>Top 500</strong> each season for about a year and joined tournaments whenever I could. During this time, I also formed a small team of scholars, 
+           whom I <strong>coached</strong> and <strong>managed</strong> alongside other groups.<br/><br/>
+          
+          In addition, I had the opportunity to merge my passion for both music and gaming by collaborating with one of the top Axie players and coaches of all
+           time, <a href="https://www.youtube.com/@ElijahMT8" target="_blank" className="text-blue-400 hover:underline visited:text-purple-400"><strong>Elijah MT8</strong> </a>
+           (a.k.a. <a href="https://open.spotify.com/artist/6mvvyrOrdUglR4tX4Vl3lb" target="_blank" className="text-blue-400 hover:underline visited:text-purple-400"><strong>Elijah Flows</strong></a>
+           ). I produced and edited the music video 
+           for <a href="https://www.youtube.com/watch?v=Ey7eeE35gjU" target="_blank" className="text-blue-400 hover:underline visited:text-purple-400"><strong>Digital Nation</strong></a> as part of this collaboration.
         </p>
         <h2 className="text-xl font-bold mb-2 tracking-wide text-gray-200">From Competitive Gamer to Content Creator</h2>
         <p>
-          (*work in progress: content creator goes here)
+          As the hype around NFT games began to fade, so did my competitive drive. Around the same time, Fortnite — a game I used to love — introduced a new mode called Zero Build. 
+           One of the main reasons I stopped playing previously was the building mechanics, so when this mode was released, I knew I’d enjoy jumping back in.<br/><br/>
+          
+          This marked the beginning of my transition from competitive gamer to <strong>content creator</strong>. I then became a <strong>Twitch streamer</strong> by 2022, primarily 
+           playing Fortnite and other shooter games. My focus shifted from competing to entertaining, having fun, and building genuine connections with my 
+           audience and fellow streamers.
         </p>
       </>
     )
@@ -27,14 +53,40 @@ const sections = [
     title: "Twitch Streaming",
     content: (
       <>
-        <h2 className="text-xl font-bold mb-2 tracking-wide text-gray-200">Twitch Milestones & Achievements</h2>
+        <h2 className="text-xl font-bold mb-2 tracking-wide text-gray-200">Streaming Journey</h2>
         <p className="mb-6">
-          (*work in progress: twitch milestones and achievements goes here)
+          I knew from the start that becoming a Fortnite Twitch streamer for a Filipino audience would be a challenge — the game wasn’t as popular locally as 
+           titles like Valorant or League of Legends. But I pursued it anyway, driven by my love for the game’s <strong>creative gameplay</strong>. Eventually, I managed 
+           to <strong>grow a healthy community</strong> and even <strong>influenced</strong> fellow streamers and gamers who hadn’t played it before to give it a try.<br/><br/>
+
+          I built everything independently — from <strong>setting up OBS Studio</strong> and <strong>customizing stream assets</strong> to <strong>implementing bots </strong>
+           and <strong>editing my own highlight reels</strong>. Over time, I grew a following of thousands, consistently streamed to hundreds of viewers per session, and hosted 
+           dozens of marathon streams, community events, and giveaways.<br/><br/>
+           
+          Even after shifting from competitive gaming to content creation, I maintained top-tier performance — reaching the <strong>highest rank every season</strong> and becoming widely recognized in the Filipino 
+           Fortnite community as the “<strong>Best Fortnite Player in the Philippines.</strong>”<br/><br/>
+           
+          Throughout this journey, I strengthened essential skills like <strong>problem solving</strong>, <strong>critical thinking</strong>, <strong>adaptability</strong>
+           , <strong>creativity</strong>, and <strong>communication</strong> — all of which continue to serve me across different disciplines today.
         </p>
-        <h2 className="text-xl font-bold mb-2 tracking-wide text-gray-200">Streaming – Tech & Tools</h2>
+        <h2 className="text-xl font-bold mb-2 tracking-wide text-gray-200">Stream Assets</h2>
+        <p className="mb-6">
+          (*work in progress: stream assets goes here)
+        </p>
+        <h2 className="text-xl font-bold mb-2 tracking-wide text-gray-200">Tech & Tools</h2>
         <p>
-          (*work in progress: streaming tech and tools goes here)
+          These are the apps and tools I have experience with as a streamer:<br/>
         </p>
+        <div className="grid grid-cols-1 md:grid-cols-2 mt-2">
+          <div className="space-y-6">
+            <strong>Streaming & Broadcast Software</strong>: OBS Studio, Streamlabs Desktop<br/>
+            <strong>Chat Bots & Stream Automation</strong>: MixItUp, NightBot, StreamElements, Streamlabs, WizeBot<br/>
+          </div>
+          <div className="space-y-6">
+            <strong>Platform & Community Tools</strong>: Twitch Creator Dashboard, Discord, X (Twitter), TikTok<br/>
+            <strong>Content Creation & Editing</strong>: Adobe Premiere Pro, Adobe Photoshop<br/>
+          </div>
+        </div>
       </>
     )
   },
@@ -65,16 +117,36 @@ const sections = [
       <>
         <h2 className="text-xl font-bold mb-2 tracking-wide text-gray-200">Gaming Coach</h2>
         <p className="mb-6">
-          (*work in progress: gaming coach goes here)
-        </p>
-        <h2 className="text-xl font-bold mb-2 tracking-wide text-gray-200">Stream Assets</h2>
-        <p className="mb-6">
-          (*work in progress: stream assets goes here)
+          Over the years, I’ve naturally taken on the role of <strong>In-Game Leader</strong> in competitive MOBA and FPS/shooter games while playing with friends and viewers. 
+           I’ve also had the opportunity to <strong>coach</strong> and <strong>mentor</strong> aspiring gamers — especially during my time as an Axie Infinity scholarship 
+           manager, where I helped others improve their gameplay and strategy.
         </p>
         <h2 className="text-xl font-bold mb-2 tracking-wide text-gray-200">Kits2g Pages and Socials</h2>
         <p>
-          (*work in progress: pages and socials goes here)
+          Feel free to follow and connect with me on social media:
         </p>
+        <div className="grid grid-cols-1 md:grid-cols-4 mt-4">
+          <div className="space-y-6 flex justify-center">
+            <a href="https://www.twitch.tv/kits2g" target="_blank" className="block w-fit px-4 py-2 bg-purple-700 text-white rounded-md hover:bg-purple-800 transition">
+              <strong>Twitch Channel</strong>
+            </a>
+          </div>
+          <div className="space-y-6 flex justify-center">
+            <a href="https://www.tiktok.com/@kits2g_ttv" target="_blank" className="block w-fit px-4 py-2 bg-[#EE1D52] text-white rounded-md hover:bg-[#c91946] transition">
+              <strong>TikTok Page</strong>
+            </a>
+          </div>
+          <div className="space-y-6 flex justify-center">
+            <a href="https://discord.gg/J9td3cpeC9" target="_blank" className="block w-fit px-4 py-2 bg-blue-600 text-white rounded-md hover:bg-blue-700 transition">
+              <strong>Discord Server</strong>
+            </a>
+          </div>
+          <div className="space-y-6 flex justify-center">
+            <a href="https://twitter.com/kits2g_ttv" target="_blank" className="block w-fit px-4 py-2 bg-gray-900 text-white rounded-md hover:bg-black transition">
+              <strong>X (Twitter)</strong>
+            </a>
+          </div>
+        </div>
       </>
     )
   }
