2d5784aa0d6f6eec365509661aa042cd2567be10
joshuakitong <joshuakitong@gmail.com>
2025-07-21 03:26:43 +0800

    docs: update webdev tech content

--- src/components/shared/tech/WebDevTech.jsx
+++ src/components/shared/tech/WebDevTech.jsx
@@ -7,12 +7,12 @@ const WebDevTech = () => {
         <div className="grid grid-cols-1 md:grid-cols-2 mt-2">
           <div className="space-y-6">
             <strong>Languages</strong>: HTML5 / CSS3, JavaScript, TypeScript, C#, Java, VB, Python<br/>
-            <strong>Frameworks & Libraries</strong>: React, AngularJS, Vue.js, Angular, ASP.NET, Flask, Kendo UI, Tailwind CSS, Framer Motion<br/>
+            <strong>Frameworks & Libraries</strong>: React, Next.js, Node.js, Vue.js, AngularJS, Angular, ASP.NET, Flask, Kendo UI, Tailwind CSS, Framer Motion<br/>
             <strong>Tools & Platforms</strong>: Git, Bitbucket, npm, Visual Studio Code<br/>
             <strong>Project Management</strong>: Jira, Confluence, Crucible
           </div>
           <div className="space-y-6">
-            <strong>Database & Backend</strong>: PL/SQL, LINQ, MySQL, Firebase (Auth, Firestore)<br/>
+            <strong>Database & Backend</strong>: PL/SQL, LINQ, MySQL, Firebase (Auth, Firestore), REST API<br/>
             <strong>Hosting & Deployment</strong>: GitHub Pages, Render, Firebase Hosting, Vercel<br/>
             <strong>AI Tools</strong>: ChatGPT, GitHub Copilot, Gemini<br/>
             <strong>Communication</strong>: Slack, Zoom, Skype
