4c84ec75a4889ab1b5303f2ff3d7bbab851fe103
joshuakitong <joshuakitong@gmail.com>
2025-07-20 22:42:00 +0800

    docs: update dev links

--- src/components/shared/links/WebDevLinks.jsx
+++ src/components/shared/links/WebDevLinks.jsx
@@ -1,6 +1,6 @@
 const WebDevLinks = () => {
     return (
-        <div className="grid grid-cols-1 md:grid-cols-4 mt-4 gap-4 mb-6">
+        <div className="grid grid-cols-1 md:grid-cols-3 mt-4 gap-4 mb-6">
           <div className="space-y-6 flex justify-center">
             <a href="https://github.com/joshuakitong" target="_blank" className="block w-full text-center px-4 py-2 bg-gray-900 text-white rounded-md hover:bg-black transition">
               <strong>GitHub</strong>
@@ -9,11 +9,6 @@ const WebDevLinks = () => {
           <div className="space-y-6 flex justify-center">
             <a href="https://www.linkedin.com/in/joshua-christopher-kitong-65805a2a9" target="_blank" className="block w-full text-center px-4 py-2 bg-blue-600 text-white rounded-md hover:bg-blue-700 transition">
               <strong>LinkedIn</strong>
-            </a>
-          </div>
-          <div className="space-y-6 flex justify-center">
-            <a href="https://leetcode.com/u/fgs8LXq4KP" target="_blank" className="block w-full text-center px-4 py-2 bg-orange-500 text-white rounded-md hover:bg-orange-600 transition">
-              <strong>LeetCode</strong>
             </a>
           </div>
           <div className="space-y-6 flex justify-center">
