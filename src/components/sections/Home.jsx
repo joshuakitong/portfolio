e7cf0d8b1c1a5adaa43c3b812247a2553f78aa6f
joshuakitong <joshuakitong@gmail.com>
2025-09-03 20:35:53 +0800

    docs: change front-end dev to full stack dev

--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -30,7 +30,7 @@ export default function Hero() {
         </h1>
 
         <p ref={titleRef} className="text-xl md:text-2xl mt-4 text-blue-500 font-semibold opacity-0">
-          Front-End Web Developer
+          Full Stack Developer
         </p>
 
         <p
